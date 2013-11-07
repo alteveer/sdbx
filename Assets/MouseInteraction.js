d180d67df315511f96b3ac6afa4ae4392bda55b1
Colin Alteveer <alteveer@gmail.com>
2013-11-07 13:06:07 -0500

    initial commit, dragdrop sort of works

---
+++ Assets/MouseInteraction.js
@@ -0,0 +1,38 @@
+﻿#pragma strict
+
+function Start () {
+
+}
+
+var mouse_down:boolean = false;
+var msk:LayerMask;
+function Update () {
+	msk = gameObject.layer;
+	
+	if(mouse_down) {
+		var ray:Ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+		var hit:RaycastHit;
+		if (Physics.Raycast (ray, hit, 1000, 13)) {
+			gameObject.transform.position = hit.point;
+			gameObject.transform.position.y += .5 * gameObject.transform.localScale.x;
+		}
+	}
+}
+
+function OnMouseOver () {
+	renderer.material.color = Color(0.1, 1, 1);
+}
+function OnMouseExit() {
+	renderer.material.color = Color(1, 1, 1);
+}
+
+function OnMouseDown() {
+	gameObject.layer = 13;
+	mouse_down = true;
+	gameObject.collider.enabled = false;
+}
+function OnMouseUp() {
+	gameObject.layer = 0;
+	mouse_down = false;
+	gameObject.collider.enabled = true;
+}
