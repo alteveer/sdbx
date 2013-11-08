8f49ce7cb494a4a6265793ee6ee0afbd49c2418b
Colin Alteveer <alteveer@gmail.com>
2013-11-08 12:42:35 -0500

    material changes, snapping to brick_size

--- Assets/MouseInteraction.js
+++ Assets/MouseInteraction.js
@@ -6,6 +6,8 @@ function Start () {
 
 var mouse_down:boolean = false;
 var msk:LayerMask;
+var brick_size = 3.0;
+
 function Update () {
 	msk = gameObject.layer;
 	
@@ -13,8 +15,13 @@ function Update () {
 		var ray:Ray = Camera.main.ScreenPointToRay(Input.mousePosition);
 		var hit:RaycastHit;
 		if (Physics.Raycast (ray, hit, 1000, 13)) {
+			gameObject.transform.rotation = Quaternion.identity;
 			gameObject.transform.position = hit.point;
-			gameObject.transform.position.y += .5 * gameObject.transform.localScale.x;
+			gameObject.transform.position.x = Mathf.Floor(gameObject.transform.position.x / brick_size) * brick_size;
+			gameObject.transform.position.y = Mathf.Floor(gameObject.transform.position.y / brick_size) * brick_size;
+			gameObject.transform.position.z = Mathf.Floor(gameObject.transform.position.z / brick_size) * brick_size;
+			
+			gameObject.transform.position.y += gameObject.transform.localScale.x;
 		}
 	}
 }
@@ -22,6 +29,7 @@ function Update () {
 function OnMouseOver () {
 	renderer.material.color = Color(0.1, 1, 1);
 }
+
 function OnMouseExit() {
 	renderer.material.color = Color(1, 1, 1);
 }
@@ -31,6 +39,7 @@ function OnMouseDown() {
 	mouse_down = true;
 	gameObject.collider.enabled = false;
 }
+
 function OnMouseUp() {
 	gameObject.layer = 0;
 	mouse_down = false;
