7a01ac1794e1b7a8db91c9bf73864d63f4c42674
Colin Alteveer <alteveer@gmail.com>
2014-02-11 22:22:56 -0500

    We can skiiiiiiii\!

--- Assets/CameraFollow.js
+++ Assets/CameraFollow.js
@@ -1,9 +1,19 @@
 ﻿#pragma strict
 
-function Start () {
+var target:GameObject;
+var offset:Vector3;
 
+function Start () {
+	if(target == null) {
+		target = GameObject.Find("/player");
+	}
+	
+	if(offset == null) {
+		offset = Vector3(0, 30, 0);
+	}
 }
 
 function Update () {
-
+	transform.position = target.transform.position + offset;
+	transform.LookAt(target.transform);
 }
--- Assets/PlayerController.js
+++ Assets/PlayerController.js
@@ -4,8 +4,35 @@ function Start () {
 
 }
 
-function Update () {
-	Input.GetAxis("RSTICK_X");
+var rotation_speed:float = 1;
+var sin_angle:float;
+
+function FixedUpdate () {
 	
-	if(
+	//Mathf.Deg2Rad * this.gameObject.transform.rotation.eulerAngles.y)) < 1) {
+	
+	if(Input.GetKey(KeyCode.LeftArrow)) {
+		transform.Rotate(0, 0, -rotation_speed);
+	}
+	if(Input.GetKey(KeyCode.RightArrow)) {
+		transform.Rotate(0, 0, rotation_speed);
+	}
+		
+	if(transform.rotation.eulerAngles.y < 270 && transform.rotation.eulerAngles.y > 179) {
+		transform.rotation = Quaternion.Euler(270, 270, 0);	
+	}
+	if(transform.rotation.eulerAngles.y > 90 && transform.rotation.eulerAngles.y < 180) {
+		transform.rotation = Quaternion.Euler(270, 90, 0);	
+	}
+	
+	sin_angle = Mathf.Sin(Mathf.Deg2Rad * this.gameObject.transform.rotation.eulerAngles.y);
+	rigidbody.drag = Mathf.Pow(Mathf.Abs(sin_angle), 4.0f);
+	
+}
+
+function OnGUI() {
+	//GUILayout.Label(Mathf.Sin(Mathf.Deg2Rad * this.gameObject.transform.rotation.eulerAngles.y).ToString("0.0"));
+	GUILayout.Label(sin_angle.ToString("0.0"));
+	GUILayout.Label(rigidbody.drag.ToString("0.0"));
+	//GUILayout.Label((Mathf.Rad2Deg * transform.localRotation.eulerAngles).ToString("0.0"));
 }
